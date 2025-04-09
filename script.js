99a76de3509e990107c33d0bf40c6e391aa23072
AshutoshRode <arode1999@gmail.com>
2025-04-09 11:47:04 +0530

    add more feature and optimize code

--- script.js
+++ script.js
@@ -4,6 +4,67 @@ let score = JSON.parse(localStorage.getItem('score')) || { wins: 0, losses: 0, t
 updateScoreElement();
 
 // null = intentionally want something to be empty 
+const rockElement = document.querySelector('.js-rock-button');
+rockElement.addEventListener('click', () => {
+    playGame('✊🏼');
+});
+
+const paperElement = document.querySelector('.js-paper-button');
+paperElement.addEventListener('click', () => {
+    playGame('🖐🏼');
+})
+
+const ScissorsElement = document.querySelector('.js-scissors-button');
+ScissorsElement.addEventListener('click', () => {
+    playGame('✌🏼');
+})
+
+
+document.body.addEventListener('keydown', (event) => {
+    // alert(event.key);
+    if (event.key === 'r') {
+        playGame('✊🏼');
+    }
+    else if (event.key === 'p') {
+        playGame('🖐🏼');
+    }
+    else if (event.key === 's') {
+        playGame('✌🏼');
+    }
+    else if (event.key === 'Backspace') {
+        showResetConfirmation();
+    }
+});
+document.querySelector('.js-reset-button').addEventListener('click', () => {
+    showResetConfirmation();
+})
+
+
+function showResetConfirmation() {
+    document.querySelector('.js-reset-confirmation').innerHTML = `
+    Are you sure you want to reset the score?
+    <button class="js-reset-confirm-yes reset-confirm-button">
+    Yes
+    </button>
+     <button class="js-reset-confirm-no reset-confirm-button">
+        No
+      </button>`;
+    document.querySelector('.js-reset-confirm-yes').addEventListener('click', () => {
+
+        hideResetConfirmation();
+        resetScore();
+    });
+
+    document.querySelector('.js-reset-confirm-no')
+        .addEventListener('click', () => {
+            hideResetConfirmation();
+        });
+
+}
+function hideResetConfirmation() {
+    document.querySelector('.js-reset-confirmation').innerHTML = '';
+}
+
 
 function playGame(playerMove) {
     const computerMove = pickComputerMove();
@@ -11,42 +72,42 @@ function playGame(playerMove) {
     let result = '';
     if (playerMove === '✌🏼') {
         if (computerMove === '✊🏼') {
-            result = 'You lose.';
+            result = 'You Lose';
         }
         else if (computerMove === '🖐🏼') {
-            result = 'You Win.';
+            result = 'You Win';
         }
         else if (computerMove === '✌🏼') {
-            result = 'Tie.';
+            result = 'Tie';
         }
     }
     else if (playerMove === '🖐🏼') {
 
         if (computerMove === '✊🏼') {
-            result = 'You Win.';
+            result = 'You Win';
         }
         else if (computerMove === '🖐🏼') {
-            result = 'Tie.';
+            result = 'Tie';
         }
         else if (computerMove === '✌🏼') {
-            result = ' You lose.';
+            result = 'You Lose';
         }
     }
     else if (playerMove === '✊🏼') {
         if (computerMove === '✊🏼') {
-            result = 'Tie.';
+            result = 'Tie';
         }
         else if (computerMove === '🖐🏼') {
-            result = 'You lose.';
+            result = 'You Lose';
         }
         else if (computerMove === '✌🏼') {
-            result = 'You Win.';
+            result = 'You Win';
         }
     }
 
     const resultElement = document.querySelector('.js-result');
 
-    if (result === 'You Win.') {
+    if (result === 'You Win') {
         // score.wins = score.wins + 1;
         score.wins += 1;
 
@@ -56,7 +117,7 @@ function playGame(playerMove) {
 
 
 
-    } else if (result === 'You lose.') {
+    } else if (result === 'You Lose') {
         // score.losses = score.losses + 1;
         score.losses += 1;
 
@@ -64,7 +125,7 @@ function playGame(playerMove) {
         resultElement.classList.remove('win');
         resultElement.classList.remove('tie');
     }
-    else if (result === 'Tie.') {
+    else if (result === 'Tie') {
         // score.ties = score.ties + 1;
         score.ties += 1;
 
@@ -114,7 +175,8 @@ function resetScore() {
     score = { wins: 0, losses: 0, ties: 0 };
     localStorage.setItem('score', JSON.stringify(score));
 }
-function resetButton(){
+
+function resetButton() {
     score.wins = 0;
     score.losses = 0;
     score.ties = 0;
@@ -124,16 +186,29 @@ function resetButton(){
 let isAutoPlaying = false;
 let intervalId;
 
-function autoPlay(){
-    if(!isAutoPlaying){
-     intervalId = setInterval(function(){
-        const playerMove = pickComputerMove();
-        playGame(playerMove);
-    },1000)
-    isAutoPlaying =true;
-}
-else{
-    clearInterval(intervalId);
-    isAutoPlaying = false;
 
-}}
+const autoPlayElement = document.querySelector(".js-auto-play-button");
+autoPlayElement.addEventListener('click', autoPlay);
+
+document.body.addEventListener('keydown', (event) => {
+    if (event.key === 'a') {
+        autoPlay();
+    }
+});
+
+function autoPlay() {
+    if (!isAutoPlaying) {
+        intervalId = setInterval(() => {
+            const playerMove = pickComputerMove();
+            playGame(playerMove);
+        }, 1000)
+        autoPlayElement.innerHTML = "Stop Playing";
+        isAutoPlaying = true;
+    }
+    else {
+        clearInterval(intervalId);
+        autoPlayElement.innerHTML = "Auto Play";
+        isAutoPlaying = false;
+
+    }
+}
